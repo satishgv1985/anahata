518f49aaac56c92614106127144a5b1b32079f62
satishgv1985 <satishgv1985@640a6214-ef9b-11de-9456-cf00f3bddd4e>
2010-03-01 11:10:10 +0000

    added news-letter.php and added french content of mobile tailoring school

---
+++ anhata/e_news-letter.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=ISO-8859-1" />
+ <title>News Letter - Anahata</title>
+    <link rel="stylesheet" type="text/css" href="css/jstyle.css" />
+</head>
+
+    <body>
+  <div class="jwrapper">
+
+<?php include("header.php"); ?>
+
+<div class="jcontent-area">
+
+ <div class="jleft-column">
+                <h2>
+                    News Letter</h2>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Buffalos
+                            </h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Village Self Independency through own Milk Animals</b>
+                            <p style="text-align: justify; padding: 5px;">
+                               Some of the families in Siddarampuram village, Anantapur (dist), Andhra Pradesh, take part in “FRIENDS”, our new Organization, intended to support women so that they can fulfill their basic needs. In this frame, they launched a village self motivated program, providing buffalos to selected poor people. By selling valuable milk they make a good income. The buffalo is a multipurpose animal: it also provides another natural source of energy! The cow dung fed to biogas pits will provide the women with a natural energy to cook. And the out residue waste from the biogas system can be converted to wormy compost which can be used for farming. As a very good side effect the alcoholic husbands give support wherever they can, for example by building grass shelters right in front of the main house for the Milk animals. Even the government of the village is so happy with this, that they wait impatiently for other projects, one of which could be solar streetlamps to provide permanent public safety through effective lighting of the streets. Indeed, the frequent and long power cuts are a big problem in India.   
+                            </p>
+                        </td>
+                        
+                    </tr>
+                    
+                </table>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Bank account for staff</h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Opening own bank account for staff</b>
+                            <p style="text-align: justify; padding: 5px;">
+                                SAI MAA SOCIETY has twenty staff, out of this only two had their own account. Now eight more staff got own account what is used to transfer salary. By providing this system the staff also learns the way of spending the money. They also learn how to save the money for their families and their future.  
+                            </p>
+                        </td>
+                        
+                    </tr>
+                   
+                </table>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Eco-Clubs </h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Installation of Eco-Clubs in 40 Village schools</b>
+                            <p style="text-align: justify; padding: 5px;">
+                                Why Eco-clubs? The extent of the damage inflected on the environment by the ongoing pollution doesn’t need to be stressed. Eco-clubs are a small effort towards combating this situation.
+<br/>
+Each one of the two Eco-Club organizers from SAI MAA SOCIETY is supporting 20 Government schools and gives training in awareness of the environment. First they teach how to collect seeds and how to keep them. Read later what they will do next.   </p>
+                        </td>
+                        
+                    </tr>
+                   
+                </table>
+            </div>
+          
+
+<?php include("quicklinks.php"); ?>
+</div>
+
+<?php include("footer.php"); ?>
+</div>
+    </body>
+    
+</html>
---
+++ anhata/news-letter.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=ISO-8859-1" />
+ <title>News Letter - Anahata</title>
+    <link rel="stylesheet" type="text/css" href="css/jstyle.css" />
+</head>
+
+    <body>
+  <div class="jwrapper">
+
+<?php include("header.php"); ?>
+
+<div class="jcontent-area">
+
+ <div class="jleft-column">
+                <h2>
+                    News Letter</h2>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Wasserbüffel
+                            </h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Dörfliche Autonomie für Siddarampuram</b>
+                            <p style="text-align: justify; padding: 5px;">
+                                Die Hälfte der Frauen in Siddarampuram, einem Dorf im Anantapur Distrikt in Andra Pradesh, Südindien, nehmen an dem Buffaloproject teil und erwerben zwei Milchkühe. Ein Projekt, das arme Familien zu Selbständigkeit und Eigenverantwortung führen soll. Durch den Verkauf dieser wertvollen Büffelmilch erwirtschaften diese Familien ein gutes Einkommen. Jede dieser 24 Familien bekommt eine eigene kleine Biogasanlage für den Kuhmist, die sie mit natürlicher Energie zum Kochen versorgt. Danach wird der Dung der Kompostanlage zugeführt und es entsteht wieder fruchtbare Erde. Höchst erfreulicher Nebeneffekt ist, dass die Ehemänner, die ja Alkoholiker sind, eifrig  mittun. So haben die Dorfbewohner auf eigene Initiative vor ihren Häusern ein Grasdach als Unterstand  für die Tiere gebaut. Auch die Bezirksverwaltung ist soweit angetan von dem Erfolg, dass sie dem Dorf Solarstrassenlampen empfiehlt, um autonom zu sein. In den indischen Dörfern gibt es bei weitem keine 24 Stunden Stromversorgung. 
+                            </p>
+                        </td>
+                        
+                    </tr>
+                    
+                </table>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Bankkonto</h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Eröffnung eigener Bankkonten für die Angestellten</b>
+                            <p style="text-align: justify; padding: 5px;">
+                                Von den 20 Angestellten der SAI MAA SOCIETY hatten nur zwei ihr eigenes Bankbuch. Acht weitere Angestellte konnten ihr eigenes Konto eröffnen, auf das seit Januar 2010 das Gehalt überwiesen wird.
+                            </p>
+                        </td>
+                        
+                    </tr>
+                   
+                </table>
+                <table style="margin-left: 16px; margin-right: 16px;">
+                    <tr>
+                        <td colspan="2">
+                            <h2>
+                                Eco-Clubs </h2>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="vertical-align: top;">
+                        <br/>
+                        <b>Einführung sogenannter Eco-Clubs in 40 Dorfschulen</b>
+                            <p style="text-align: justify; padding: 5px;">
+                                Warum Eco-Clubs? Die Umweltverschmutzung und deren Schäden haben Weltweit ein solches Ausmass angenommen, dass auch wir unseren kleinen Beitrag zu dessen Behebung leisten wollen.
+<br/>
+Jeder der beiden Eco-Club Aktivisten der SAI MAA SOCIETY hat  20 Dorfschulen zu betreuen in Sachen Umweltfragen, wie Samen sammeln und konservieren, Errichtung einer Schuleigenen Kompostanlage, Achtsamkeit gegen Verschmutzung. 
+                            </p>
+                        </td>
+                        
+                    </tr>
+                   
+                </table>
+            </div>
+          
+
+<?php include("quicklinks.php"); ?>
+</div>
+
+<?php include("footer.php"); ?>
+</div>
+    </body>
+    
+</html>
