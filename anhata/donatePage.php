1adf6ee7b3152f57f04ba35cee80a926a363b8fe
satishgv1985 <satishgv1985@640a6214-ef9b-11de-9456-cf00f3bddd4e>
2010-04-09 12:56:25 +0000

    added donate for all languages and added images

---
+++ anhata/donatePage.php
@@ -0,0 +1,142 @@
+<table cellspacing="6">
+	<tr>
+		<td><b> Spendenbestätigung: </b></td>
+	</tr>
+	<tr>
+		<td>Der Verein ist beim Finanzamt als gemeinnützig anerkannt und
+		stellt Spendenbescheinigungen aus. Sie können auch einzelne Projekte
+		fördern.</td>
+	</tr>
+	<tr>
+		<td height="5"></td>
+	</tr>
+	<tr>
+		<td><b>Spendenkonto Anahata e.V.</b></td>
+	</tr>
+	<tr>
+		<td>
+		<table cellpadding="2" cellspacing="5" width="300">
+			<tr>
+				<td width="50">Konto-Nr.:</td>
+
+				<td>835 703 806</td>
+			</tr>
+			<tr>
+				<td>BLZ.:</td>
+				<td>700 100 80</td>
+			</tr>
+			<tr>
+				<td>Bank:</td>
+				<td>Postbank München</td>
+			</tr>
+			<tr>
+				<td>IBAN:</td>
+				<td>DE26700100800835703806</td>
+			</tr>
+			<tr>
+				<td>BIC:</td>
+				<td>PBNKDEFF700</td>
+
+			</tr>
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td height="5"></td>
+	</tr>
+	<tr>
+		<td><b>Kontakt und Impressum: </b></td>
+	</tr>
+	<tr>
+		<td>
+		<table cellspacing="5" width="100%">
+			<tr>
+				<td>Monika Müller</td>
+			</tr>
+			<tr>
+				<td>Siegenburgerstrasse 31</td>
+			</tr>
+			<tr>
+				<td>D - 81373 München</td>
+			</tr>
+			<tr>
+				<td>Telefon: +49 (0) 89 / 769 83 03</td>
+			</tr>
+			<tr>
+				<td>Telefax: +49 (0) 89 / 769 83 97</td>
+			</tr>
+			<tr>
+				<td>E-mail: <a class="link1"
+					href="mailto:monika.mueller.indienhilfe@anahata-ev.de">monika.mueller.indienhilfe
+				@ anahata-ev.de </a></td>
+			</tr>
+
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td height="5"></td>
+	</tr>
+	<tr>
+		<td><b>Vertretungsberechtigte für den Verein:</b></td>
+	</tr>
+	<tr>
+		<td>
+		<table cellspacing="5" width="300">
+
+
+			<tr>
+				<td>Monika Müller</td>
+			</tr>
+			<tr>
+				<td>Gudula Blau</td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+	<tr>
+		<td height="5"></td>
+	</tr>
+
+	<tr>
+		<td><b>Amtsgericht München - Registergericht </b></td>
+	</tr>
+
+	<tr>
+		<td>
+		<table cellspacing="5" width="500">
+
+
+			<tr>
+				<td>Eintragung im Vereinsregister München</td>
+			</tr>
+			<tr>
+				<td>Betreff: <b>Anahata e.V., Sitz: München, VR 18181, Fall Nr. 1</b></td>
+			</tr>
+		</table>
+		</td>
+	</tr>
+
+	<tr>
+		<td height="5"></td>
+	</tr>
+	<tr>
+		<td><b>Rechtlicher Hinweis: </b></td>
+	</tr>
+	<tr>
+		<td>
+		<p style="text-align: justify">Der Verein Anahata e.V. übernimmt keine
+		Haftung oder Garantie für die Aktualität, Vollständigkeit und
+		Richtigkeit der auf dieser Website bereitgestellten Informationen und
+		behält sich das Recht vor, ohne vorherige Ankündigung Änderungen oder
+		Ergänzungen der bereitgestellten Informationen vorzunehmen. Der Verein
+		Anahata e.V.haftet nicht für direkte oder indirekte Schäden,
+		einschließlich entgangenen Gewinns, die aufgrund von - oder sonst wie
+		in Verbindung mit - Informationen entstehen, die auf dieser Website
+		bereitgehalten werden. Der Verein Anahata e.V. ist für die Inhalte
+		fremder Seiten, die über einen externen Link erreicht werden, nicht
+		verantwortlich. Diese Inhalte sind keine eigenen Inhalte des Vereins
+		Anahata e.V.</p>
+		</td>
+	</tr>
+</table>
